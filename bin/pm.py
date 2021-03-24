b5ffb1eb2eed50f8deb13beb7f4edad668b169e0
Acmpo6ou <acmpo6ou@gmail.com>
2021-03-24 17:25:21 +0200

    Rewrite pomodoro timer in python
    Because bash's command `sleep` doesn't support pausing and resuming the timer properly

---
+++ bin/pm.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import time, os
+
+time.sleep(1800)  # 30 minutes
+os.system("cinnamon-screensaver-command --lock")
--- bin/pm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sleep 1800 # 30 minutes
-cinnamon-screensaver-command --lock
