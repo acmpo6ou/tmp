8c40b5fa522af4d9ac9db5297ae5fa83de14b398
Acmpo6ou <acmpo6ou@gmail.com>
2021-06-23 09:18:00 +0300

    Add helper function to input matrices

--- rc/ipy-startup.py
+++ rc/ipy-startup.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+def input_matrix():
+    n, m = map(int, input().split())  # taking number of rows and column
+    return np.array([input().strip().split() for _ in range(n)], int)
