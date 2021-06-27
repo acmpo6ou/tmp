83b1f76fd1edb8745ac14d062cc356b9623e3fea
Acmpo6ou <acmpo6ou@gmail.com>
2021-06-27 15:43:44 +0300

    Fix input_matrix to have data_type argument

--- rc/ipy-startup.py
+++ rc/ipy-startup.py
@@ -3,6 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def input_matrix():
+def input_matrix(data_type=float):
     n, m = map(int, input().split())  # taking number of rows and column
-    return np.array([input().strip().split() for _ in range(n)], int)
+    return np.array([input().strip().split() for _ in range(n)], data_type)
