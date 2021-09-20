8f47d717c95fa92e31b363c27081fe8db7e54cd5
Acmpo6ou <acmpo6ou@gmail.com>
2021-09-20 18:39:58 +0300

    Add mime type for rust

--- MIME/install.sh
+++ MIME/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp x-kotlin.xml /usr/share/mime/packages
+cp rust.xml /usr/share/mime/packages
 cp -r mimetypes /usr/share/icons/Mint-Y-Dark
 update-mime-database /usr/share/mime
 sudo update-icon-caches /usr/share/icons
