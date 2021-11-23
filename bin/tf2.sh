b02fbdcaf3f971f5dd2385579d6ca10a4cb064e8
acmpo6ou <acmpo6ou@gmail.com>
2021-11-23 14:39:24 +0200

    Add tf2 shourtcuts to launch it with or without custom

---
+++ bin/tf2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv "$HOME/.steam/steam/steamapps/common/Team Fortress 2/tf/custom2" "$HOME/.steam/steam/steamapps/common/Team Fortress 2/tf/custom"
+~/Documents/tmp/bin/walls_tf2.py
+steam steam://rungameid/440
---
+++ bin/tf2_no_custom.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv "$HOME/.steam/steam/steamapps/common/Team Fortress 2/tf/custom" "$HOME/.steam/steam/steamapps/common/Team Fortress 2/tf/custom2"
+steam steam://rungameid/440
