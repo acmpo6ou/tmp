d80c9893f999e9de1fbb27d282813bdc94e15c2a
acmpo6ou <acmpo6ou@gmail.com>
2021-11-30 18:21:38 +0200

    Add hash sum printing to crossover.sh

--- bin/crossover.sh
+++ bin/crossover.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo rm /opt/cxoffice/lib/wine/winewrapper.exe.so
 sudo cp winewrapper.exe.so /opt/cxoffice/lib/wine/winewrapper.exe.so
+
+md5sum /opt/cxoffice/lib/wine/winewrapper.exe.so
+md5sum winewrapper.exe.so
